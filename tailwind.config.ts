428590f5c2e3ddf57e341aa75048537855c5af2c
yoorae <114637091+yrk721@users.noreply.github.com>
2024-07-03 17:04:42 +0900

    Feat/#3 디자인 시스템 세팅 (#7)
    * feat: 타이포그래피 설정 (#3)
    
    * feat: 타이포그래피 변경 사항 추가 (#3)
    
    * style: 컬러시스템 추가 (#3)
    
    * chore: 폰트 설정 추가 (#3)
    
    * feat: localFont 설정 추가 (#3)
    
    * feat: 추가된 색상 DEFAULT 설정 추가 (#3)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   darkMode: ['class'],
@@ -18,6 +19,32 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['96px', '1.2'],
+        h2: ['60px', '1.3'],
+        h3: ['48px', '1.4'],
+        h4: ['36px', '1.5'],
+        body1: ['28px', '1.6'],
+        body2: ['24px', '1.7'],
+        body3: ['24px', '1.7'],
+        body4: ['22px', '1.8'],
+        body5: ['22px', '1.8'],
+        body6: ['20px', '1.8'],
+        body7: ['20px', '1.8'],
+        body8: ['18px', '1.8'],
+        mobile1: ['16px', '1.9'],
+        mobile2: ['14px', '2.0'],
+        caption: ['12px', '2.1'],
+      },
+      fontWeight: {
+        bold: '700',
+        semibold: '600',
+        medium: '500',
+        regular: '400',
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
@@ -52,6 +79,24 @@ const config: Config = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        danger: {
+          DEFAULT: '#FF4235',
+          50: '#FEF2F2',
+          500: '#FF4235',
+        },
+        success: {
+          DEFAULT: '#00B45E',
+          50: '#E2F7F0',
+          500: '#00B45E',
+        },
+        info: {
+          DEFAULT: '#0064FF',
+          50: '#E6F0FF',
+          500: '#0064FF',
+        },
+        errorclicked: '#7D032F',
+        black: '#000000',
+        white: '#FFFFFF',
       },
       borderRadius: {
         lg: 'var(--radius)',
@@ -74,7 +119,90 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    require('tailwindcss-animate'),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.headline1': {
+          fontSize: '96px',
+          fontWeight: '700',
+          lineHeight: '1.2',
+        },
+        '.headline2': {
+          fontSize: '60px',
+          fontWeight: '700',
+          lineHeight: '1.3',
+        },
+        '.headline3': {
+          fontSize: '48px',
+          fontWeight: '700',
+          lineHeight: '1.4',
+        },
+        '.headline4': {
+          fontSize: '36px',
+          fontWeight: '700',
+          lineHeight: '1.5',
+        },
+        '.body1': {
+          fontSize: '28px',
+          fontWeight: '600',
+          lineHeight: '1.6',
+        },
+        '.body2': {
+          fontSize: '24px',
+          fontWeight: '600',
+          lineHeight: '1.7',
+        },
+        '.body3': {
+          fontSize: '24px',
+          fontWeight: '500',
+          lineHeight: '1.7',
+        },
+        '.body4': {
+          fontSize: '22px',
+          fontWeight: '500',
+          lineHeight: '1.8',
+        },
+        '.body5': {
+          fontSize: '22px',
+          fontWeight: '400',
+          lineHeight: '1.8',
+        },
+        '.body6': {
+          fontSize: '20px',
+          fontWeight: '600',
+          lineHeight: '1.8',
+        },
+        '.body7': {
+          fontSize: '20px',
+          fontWeight: '500',
+          lineHeight: '1.8',
+        },
+        '.body8': {
+          fontSize: '18px',
+          fontWeight: '500',
+          lineHeight: '1.8',
+        },
+        '.mobile1': {
+          fontSize: '16px',
+          fontWeight: '500',
+          lineHeight: '1.9',
+        },
+        '.mobile2': {
+          fontSize: '14px',
+          fontWeight: '500',
+          lineHeight: '2.0',
+        },
+        '.caption': {
+          fontSize: '12px',
+          fontWeight: '400',
+          lineHeight: '2.1',
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 };
 
 export default config;
