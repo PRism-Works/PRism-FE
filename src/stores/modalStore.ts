f20adc8e1e39f2d7aa83e45749baf119f33f4b9a
yrk721 <yoorae0721@naver.com>
2024-07-04 20:48:13 +0900

    feat: ModalState 및 useModal hook 생성 (#4)

---
+++ src/stores/modalStore.ts
@@ -0,0 +1,13 @@
+import create from 'zustand';
+
+interface ModalState {
+  modals: React.ReactNode[];
+  openModal: (modal: React.ReactNode) => void;
+  closeModal: () => void;
+}
+
+export const useModalStore = create<ModalState>((set) => ({
+  modals: [],
+  openModal: (modal) => set((state) => ({ modals: [...state.modals, modal] })),
+  closeModal: () => set((state) => ({ modals: state.modals.slice(0, -1) })),
+}));
