dd3cbba04fdcd8f201e525232abedacc084228e4
yrk721 <yoorae0721@naver.com>
2024-07-06 07:26:29 +0900

    feat: ConfirmModal 내부 DialogContent 스크롤 처리 (10)

--- src/components/modal/confirmModal/ConfirmModal.tsx
+++ src/components/modal/confirmModal/ConfirmModal.tsx
@@ -13,14 +13,22 @@ interface ConfirmModalProps {
   title?: string;
   children?: React.ReactNode;
   footer?: React.ReactNode;
+  showCloseButton?: boolean;
 }
 
-export default function ConfirmModal({ title, children, footer }: ConfirmModalProps) {
+export default function ConfirmModal({
+  title,
+  children,
+  footer,
+  showCloseButton = true,
+}: ConfirmModalProps) {
   const closeModal = useModalStore((state) => state.closeModal);
 
   return (
     <Dialog open onOpenChange={closeModal}>
-      <DialogContent className="p-11">
+      <DialogContent
+        className="max-h-[85vh] overflow-y-auto p-11"
+        showCloseButton={showCloseButton}>
         <DialogHeader className="flex flex-col items-center text-center">
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
