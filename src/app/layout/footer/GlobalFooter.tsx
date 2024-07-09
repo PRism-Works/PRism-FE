dd334ebbfe621bf36ac4df0619d9fbaab3d09911
yrk721 <yoorae0721@naver.com>
2024-07-09 20:24:25 +0900

    feat: GlobalFooter로 UI 제작 (#21)

---
+++ src/app/layout/footer/GlobalFooter.tsx
@@ -0,0 +1,10 @@
+export default function GlobalFooter() {
+  return (
+    <footer className="fixed bottom-0 w-full py-6 md:py-0">
+      <div className="flex flex-col items-center justify-center gap-4 md:h-20 md:flex-row">
+        <p className="text-gray-600 body6">PRism</p>
+        <p className="text-gray-400 display5">©PRism. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
