ac19d332fe442028eb4734c263a2fd7332e17b71
yrk721 <yoorae0721@naver.com>
2024-07-09 20:28:32 +0900

    feat: HomePage 스크린 제작 (#21)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,15 @@
-import Header from '../layout/header/Header';
+'use client';
+
+import ProjectRegisterButton from '@/components/domain/project/projectRegister/ProjectRegisterButton';
+import ProjectSearchBar from '@/components/domain/project/projectSearch/ProjectSearchBar';
 
 export default function HomePage() {
-  return <Header />;
+  return (
+    <div className="container mx-auto">
+      <ProjectRegisterButton className="mb-[140px] mr-[150px] mt-[445px] flex justify-end" />
+      <div className="flex w-full justify-center">
+        <ProjectSearchBar className="w-full max-w-[690px]" />
+      </div>
+    </div>
+  );
 }
