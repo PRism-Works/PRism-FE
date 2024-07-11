c8ddddbcf91a271052ef27833e44d4d9179d7e03
yrk721 <yoorae0721@naver.com>
2024-07-11 15:32:49 +0900

    feat: 비밀번호 정규식 수정 및 인증번호 알림메세지 수정 (#16)

--- src/models/authModels.tsx
+++ src/models/authModels.tsx
@@ -14,7 +14,7 @@ export const SignupSchema = z
     certification: z
       .string()
       .length(4, '인증번호는 4자리 숫자여야 합니다.')
-      .regex(/^\d{4}$/, '인증번호는 숫자 4자리여야 합니다.'),
+      .regex(/^\d{4}$/, '인증번호는 4자리 숫자여야 합니다.'),
     password: z
       .string()
       .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
@@ -24,10 +24,13 @@ export const SignupSchema = z
         '비밀번호는 영문, 숫자, 특수문자 중 2가지 이상을 포함해야 합니다.',
       )
       .refine(
-        (val) => !/(.)\1\1/.test(val),
+        (val) => !/(.)\1{2}/.test(val),
         '비밀번호에 동일한 문자가 3회 이상 반복될 수 없습니다.',
       )
-      .refine((val) => !/\d{3}/.test(val), '비밀번호에 연속된 숫자가 3개 이상 포함될 수 없습니다.'),
+      .refine(
+        (val) => !/(\d)\1{2}/.test(val),
+        '비밀번호에 연속된 숫자가 3개 이상 포함될 수 없습니다.',
+      ),
     verifyPassword: z.string(),
   })
   .superRefine(({ password, verifyPassword }, ctx) => {
