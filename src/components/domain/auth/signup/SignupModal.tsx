8eefc606e300fcd85b6d0a18fffbbd18e80e060b
yoorae <114637091+yrk721@users.noreply.github.com>
2024-07-11 16:24:07 +0900

    Feat/#16 회원가입/로그인 유효성 검사 및 세부기능 추가 (#28)
    * feat: authModels에 SignupSchema/LoginSchema 생성 (#16)
    
    * feat: ConfirmButton에 onClick, disabled 속성 추가 (#16)
    
    * feat: 회원가입 모달 유효성 검사 로직 추가 (#16)
    
    * feat: 로그인 모달 유효성 검사 로직 추가 (#16)
    
    * style: formDescription 및 formMessage 폰트 caption으로 변경 (#16)
    
    * style: 로고 합친 버전으로 변경 (#16)
    
    * chore: 기존 로고 삭제 (#16)
    
    * feat: 비밀번호 정규식 수정 및 인증번호 알림메세지 수정 (#16)
    
    * feat: useReducer hook으로 변경 (#16)
    
    * feat: FormProvider 제거 (#16)

--- src/components/domain/auth/signup/SignupModal.tsx
+++ src/components/domain/auth/signup/SignupModal.tsx
@@ -1,99 +1,234 @@
 'use client';
 
-import React, { useId } from 'react';
+import React, { useId, useReducer } from 'react';
 import ModalLayout from '@/components/modal/ModalLayout';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SignupSchema, SignupForm } from '@/models/authModels';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { PasswordInput } from '@/components/common/input/PasswordInput';
 import { CheckCircle2 } from 'lucide-react';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 export default function SignupModal() {
   const id = useId();
   const isSmallScreen = useMediaQuery('(max-width: 430px)');
+  const [isAgreed, setIsAgreed] = useReducer((state) => !state, false);
+
+  const formMethods = useForm<SignupForm>({
+    mode: 'onChange',
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      certification: '',
+      password: '',
+      verifyPassword: '',
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = formMethods;
+
+  // NOTE: alert는 api 연동하면서 수정할 예정입니다. 현재는 data를 담고 있습니다.
+  const onSubmit = (data: SignupForm) => {
+    alert(JSON.stringify(data));
+  };
+
+  const email = watch('email');
+  const certification = watch('certification');
+
+  const isEmailValid = !errors.email && email.length > 0;
+  const isCertificationValid = !errors.certification && certification.length === 4;
 
   return (
     <ModalLayout
       title="회원가입"
-      footer={<ModalLayout.ConfirmButton title="회원가입하기" isSmallScreen={isSmallScreen} />}>
-      <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-name`}>
-          이름
-        </Label>
-        <Input type="text" id={`${id}-signup-name`} placeholder="이름" className="w-full" />
-      </div>
-      <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-email`}>
-          이메일 주소
-        </Label>
-        <div className="mb-2 flex flex-col items-center justify-between sm:flex-row">
-          <Input
-            type="email"
-            id={`${id}-signup-email`}
-            placeholder="prism12@gmail.com"
-            className="w-full flex-grow sm:w-auto"
-          />
-          <Button className="mt-2 h-[45px] w-full bg-purple-200 mobile1 hover:bg-purple-400 sm:ml-2 sm:mt-0 sm:w-auto">
-            중복확인
-          </Button>
-        </div>
-      </div>
-      <div className="mt-4 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-certification`}>
-          인증번호
-        </Label>
-        <div className="mb-2 flex flex-col items-center justify-between sm:flex-row">
-          <Input
-            type="text"
-            id={`${id}-signup-certification`}
-            placeholder="0000"
-            className="w-full flex-grow sm:w-auto"
-          />
-          <Button className="mt-2 h-[45px] w-full bg-purple-200 mobile1 hover:bg-purple-400 sm:ml-2 sm:mt-0 sm:w-auto">
-            인증하기
-          </Button>
-        </div>
-      </div>
-
-      <div className="mt-4 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-password`}>
-          비밀번호
-        </Label>
-        <PasswordInput id={`${id}-signup-password`} placeholder="비밀번호" className="w-full" />
-      </div>
-
-      <div className="mb-5 mt-6 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-verify-password`}>
-          비밀번호 확인
-        </Label>
-        <PasswordInput
-          id={`${id}-signup-verify-password`}
-          placeholder="비밀번호 확인"
-          className="w-full"
+      footer={
+        <ModalLayout.ConfirmButton
+          title="회원가입하기"
+          isSmallScreen={isSmallScreen}
+          onClick={handleSubmit(onSubmit)}
+          disabled={!isValid || !isAgreed}
         />
-      </div>
+      }>
+      <Form {...formMethods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    이름
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id={`${id}-signup-name`}
+                      placeholder="이름"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.name?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
 
-      <div className={`w-full max-w-[420px] ${isSmallScreen ? 'caption' : 'display5'}`}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <CheckCircle2 className="h-7 w-7 cursor-pointer text-gray-400" />
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    이메일 주소
+                  </FormLabel>
+                  <div className="flex flex-col items-center justify-between sm:flex-row">
+                    <FormControl>
+                      <Input
+                        type="email"
+                        id={`${id}-signup-email`}
+                        placeholder="prism12@gmail.com"
+                        {...field}
+                        className="w-full flex-grow sm:w-auto"
+                      />
+                    </FormControl>
+                    <Button
+                      className="h-[45px] w-full bg-purple-500 display6 hover:bg-purple-600 sm:ml-2 sm:mt-0 sm:w-auto"
+                      disabled={!isEmailValid}>
+                      중복확인
+                    </Button>
+                  </div>
+                  <FormMessage>{errors.email?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
           </div>
-          <div>
-            필수동의 항목 및 개인정보 수집 및 이용 동의 및 이메일 정보 수신에 모두 동의합니다.
+
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="certification"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    인증번호
+                  </FormLabel>
+                  <div className="flex flex-col items-center justify-between sm:flex-row">
+                    <FormControl>
+                      <Input
+                        type="text"
+                        id={`${id}-signup-certification`}
+                        placeholder="0000"
+                        {...field}
+                        className="w-full flex-grow sm:w-auto"
+                      />
+                    </FormControl>
+                    <Button
+                      className="h-[45px] w-full bg-purple-500 display6 hover:bg-purple-600 sm:ml-2 sm:mt-0 sm:w-auto"
+                      disabled={!isCertificationValid}>
+                      인증하기
+                    </Button>
+                  </div>
+                  <FormMessage>{errors.certification?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
           </div>
-        </div>
-      </div>
+
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    비밀번호
+                  </FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      id={`${id}-signup-password`}
+                      placeholder="비밀번호"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.password?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="mb-6 mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="verifyPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    비밀번호 확인
+                  </FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      id={`${id}-signup-verify-password`}
+                      placeholder="비밀번호 확인"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.verifyPassword?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className={`w-full max-w-[420px] ${isSmallScreen ? 'caption' : 'display5'}`}>
+            <div className="flex items-center justify-between">
+              <div className="mr-3" onClick={setIsAgreed}>
+                <CheckCircle2
+                  className={`h-7 w-7 cursor-pointer stroke-[1.5px] ${
+                    isAgreed ? 'text-purple-500' : 'text-gray-400'
+                  }`}
+                />
+              </div>
+
+              <p className="text-sm text-muted-foreground md:text-left">
+                필수동의 항목 및{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer text-info underline underline-offset-4">
+                  개인정보 수집 및 이용 동의
+                </a>{' '}
+                및{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer text-info underline underline-offset-4">
+                  이메일 정보 수신
+                </a>
+                에 모두 동의합니다.
+              </p>
+            </div>
+          </div>
+        </form>
+      </Form>
     </ModalLayout>
   );
 }
--- src/components/domain/project/projectRegisterModal/step/Step1.tsx
+++ src/components/domain/project/projectRegisterModal/step/Step1.tsx
@@ -29,8 +29,8 @@ export default function Step1() {
         render={({ field }) => (
           <FormItem className="mb-[28px]">
             <FormLabel className="text-purple-500 mobile1">프로젝트 명*</FormLabel>
-            <FormDescription className="text-gray-500 caption">
-              서비스 이름 혹은 팀명을 입력해 주세요
+            <FormDescription className="text-gray-500">
+              서비스 이름 혹은 팀명을 입력해 주세요.
             </FormDescription>
             <div className="relative">
               <FormControl>
@@ -40,7 +40,7 @@ export default function Step1() {
                   {...field}
                 />
               </FormControl>
-              <FormMessage className="absolute bottom-[-20px] left-0 text-danger-500 caption" />
+              <FormMessage className="absolute bottom-[-20px] left-0 text-danger-500" />
             </div>
           </FormItem>
         )}
@@ -51,8 +51,8 @@ export default function Step1() {
         render={({ field }) => (
           <FormItem className="mb-[28px]">
             <FormLabel className="mobile1">기관명</FormLabel>
-            <FormDescription className="text-gray-500 caption">
-              주최측 혹은 팀이 속한 커뮤니티를 입력해 주세요
+            <FormDescription className="text-gray-500">
+              주최 측 혹은 팀이 속한 커뮤니티를 입력해 주세요.
             </FormDescription>
             <FormControl>
               <Input className={`w-full`} placeholder="기관명" {...field} />
@@ -67,13 +67,13 @@ export default function Step1() {
         render={({ field }) => (
           <FormItem>
             <FormLabel className="text-purple-500 mobile1">프로젝트 기간*</FormLabel>
-            <FormDescription className="text-gray-500 caption">
-              프로젝트 기간을 입력해 주세요
+            <FormDescription className="text-gray-500">
+              프로젝트 기간을 입력해 주세요.
             </FormDescription>
             <FormControl>
               <Input type="date" className={`w-full ${getErrorClass('start_date')}`} {...field} />
             </FormControl>
-            <FormMessage className="text-danger-500 caption" />
+            <FormMessage className="text-danger-500" />
           </FormItem>
         )}
       />
