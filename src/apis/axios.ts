b02be94bb7f6b1e755cbc82ca6252f53c96693d9
yrk721 <yoorae0721@naver.com>
2024-07-11 22:08:23 +0900

    feat: Axios interceptor 설정 (#20)

---
+++ src/apis/axios.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers = config.headers || {};
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export const ax = instance;
