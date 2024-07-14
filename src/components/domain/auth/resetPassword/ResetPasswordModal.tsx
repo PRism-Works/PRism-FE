b80e4235c219c4137cfb75bfd0d9881180033fd3
yrk721 <yoorae0721@naver.com>
2024-07-14 16:09:03 +0900

    feat: ResetPasswordSchema 및 유효성 검사 기능 추가 (#34)

--- src/components/domain/auth/resetPassword/ResetPasswordModal.tsx
+++ src/components/domain/auth/resetPassword/ResetPasswordModal.tsx
@@ -1,90 +1,207 @@
 'use client';
 
 import React, { useId } from 'react';
+import ModalLayout from '@/components/common/modal/ModalLayout';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ResetPasswordSchema, ResetPasswordForm } from '@/models/authModels';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { PasswordInput } from '@/components/common/input/PasswordInput';
-import ModalLayout from '@/components/common/modal/ModalLayout';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 export default function ResetPasswordModal() {
   const id = useId();
   const isSmallScreen = useMediaQuery('(max-width: 430px)');
+
+  const formMethods = useForm<ResetPasswordForm>({
+    mode: 'onChange',
+    resolver: zodResolver(ResetPasswordSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      certification: '',
+      newPassword: '',
+      verifyNewPassword: '',
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = formMethods;
+
+  // NOTE: api 연동하면서 수정할 예정입니다.
+  const onSubmit = (data: ResetPasswordForm) => {
+    alert(JSON.stringify(data));
+  };
+
+  const email = watch('email');
+  const certification = watch('certification');
+
+  const isEmailValid = !errors.email && email.length > 0;
+  const isCertificationValid = !errors.certification && certification.length === 4;
 
   return (
     <ModalLayout
       contentClassName="max-w-[500px]"
       title="비밀번호 재설정"
       footer={
-        <ModalLayout.ConfirmButton title="비밀번호 변경하기" isSmallScreen={isSmallScreen} />
-      }>
-      <div className="mt-10 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-name`}>
-          이름
-        </Label>
-        <Input type="text" id={`${id}-signup-name`} placeholder="이름" className="w-full" />
-      </div>
-      <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-email`}>
-          이메일 주소
-        </Label>
-        <div className="flex flex-col items-center justify-between sm:flex-row">
-          <Input
-            type="email"
-            id={`${id}-signup-email`}
-            placeholder="prism12@gmail.com"
-            className="w-full flex-grow sm:w-auto"
-          />
-          <Button className="mt-2 h-[45px] w-full bg-purple-200 mobile1 hover:bg-purple-400 sm:ml-2 sm:mt-0 sm:w-auto">
-            인증번호 받기
-          </Button>
-        </div>
-      </div>
-      <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-certification`}>
-          인증번호
-        </Label>
-        <div className="flex flex-col items-center justify-between sm:flex-row">
-          <Input
-            type="text"
-            id={`${id}-signup-certification`}
-            placeholder="0000"
-            className="w-full flex-grow sm:w-auto"
-          />
-          <Button className="mt-2 h-[45px] w-full bg-purple-200 mobile1 hover:bg-purple-400 sm:ml-2 sm:mt-0 sm:w-auto">
-            인증하기
-          </Button>
-        </div>
-      </div>
-
-      <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-password`}>
-          새 비밀번호
-        </Label>
-        <PasswordInput id={`${id}-signup-password`} placeholder="비밀번호" className="w-full" />
-      </div>
-
-      <div className="mb-4 mt-8 grid w-full max-w-[420px] items-center gap-1">
-        <Label
-          className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
-          htmlFor={`${id}-signup-verify-password`}>
-          비밀번호 확인
-        </Label>
-        <PasswordInput
-          id={`${id}-signup-verify-password`}
-          placeholder="비밀번호 확인"
-          className="w-full"
+        <ModalLayout.ConfirmButton
+          title="비밀번호 변경하기"
+          isSmallScreen={isSmallScreen}
+          onClick={handleSubmit(onSubmit)}
+          disabled={!isValid}
         />
-      </div>
+      }>
+      <Form {...formMethods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-8 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    이름
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id={`${id}-reset-password-name`}
+                      placeholder="이름"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.name?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    이메일 주소
+                  </FormLabel>
+                  <div className="flex flex-col items-center justify-between sm:flex-row">
+                    <FormControl>
+                      <Input
+                        type="email"
+                        id={`${id}-reset-password-email`}
+                        placeholder="prism12@gmail.com"
+                        {...field}
+                        className="w-full flex-grow sm:w-auto"
+                      />
+                    </FormControl>
+                    <Button
+                      className="h-[45px] w-full bg-purple-500 display6 hover:bg-purple-600 sm:ml-2 sm:mt-0 sm:w-auto"
+                      disabled={!isEmailValid}>
+                      인증번호 받기
+                    </Button>
+                  </div>
+                  <FormMessage>{errors.email?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="certification"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}>
+                    인증번호
+                  </FormLabel>
+                  <div className="flex flex-col items-center justify-between sm:flex-row">
+                    <FormControl>
+                      <Input
+                        type="text"
+                        id={`${id}-reset-password-certification`}
+                        placeholder="0000"
+                        {...field}
+                        className="w-full flex-grow sm:w-auto"
+                      />
+                    </FormControl>
+                    <Button
+                      className="h-[45px] w-full bg-purple-500 display6 hover:bg-purple-600 sm:ml-2 sm:mt-0 sm:w-auto"
+                      disabled={!isCertificationValid}>
+                      인증하기
+                    </Button>
+                  </div>
+                  <FormMessage>{errors.certification?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="mt-6 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="newPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel
+                    className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
+                    htmlFor={`${id}-new-password`}>
+                    새 비밀번호
+                  </FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      id={`${id}-new-password`}
+                      placeholder="비밀번호"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.newPassword?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="mb-4 mt-8 grid w-full max-w-[420px] items-center gap-1">
+            <FormField
+              control={formMethods.control}
+              name="verifyNewPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel
+                    className={`text-black ${isSmallScreen ? 'mobile2' : 'mobile1'}`}
+                    htmlFor={`${id}-verify-new-password`}>
+                    비밀번호 확인
+                  </FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      id={`${id}-verify-new-password`}
+                      placeholder="비밀번호 확인"
+                      {...field}
+                      className="w-full"
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.verifyNewPassword?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </ModalLayout>
   );
 }
