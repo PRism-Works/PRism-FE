6126195e86622f9cc2fc61f77fd36034a307dff4
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-16 02:09:01 +0900

    feat: Step2 footer 안내메시지 추가 (#37)

--- src/components/domain/project/projectRegisterModal/layout/ProjectRegisterFooter.tsx
+++ src/components/domain/project/projectRegisterModal/layout/ProjectRegisterFooter.tsx
@@ -23,26 +23,36 @@ export default function ProjectRegisterFooter({
   const isLastStep = currStep === MAX_STEP;
 
   return (
-    <div className="flex w-full items-center justify-center">
-      <div className="flex-1" /> {/* 왼쪽 여백 */}
-      <div className="gap-4 flex-center">
-        <ArrowLeftCircle
-          className={getButtonClass(currStep > 0)}
-          onClick={currStep > 0 ? handlePrevStep : undefined}
-        />
-        <ArrowRightCircle
-          className={getButtonClass(isValid && currStep < MAX_STEP)}
-          onClick={!isLastStep ? handleNextStep : undefined}
-        />
-      </div>
-      <div className="flex flex-1 justify-end">
-        {isLastStep && (
-          <Button
-            onClick={handleExternalSubmit}
-            className="rounded-[10px] bg-purple-500 px-[16px] py-[10px] text-white mobile1 hover:bg-purple-600">
-            등록하기
-          </Button>
-        )}
+    <div className="w-full gap-1 flex-col-center">
+      {currStep === 1 && (
+        <div className="text-gray-600 caption">
+          <span className="text-info-500">메일 주소</span>
+          <span>로 팀원을 확인하기 때문에 </span>
+          <span className="text-info-500">한 번 더 확인해 주세요!</span>
+        </div>
+      )}
+
+      <div className="flex w-full items-center">
+        <div className="flex-1" /> {/* 왼쪽 여백 */}
+        <div className="gap-4 flex-center">
+          <ArrowLeftCircle
+            className={getButtonClass(currStep > 0)}
+            onClick={currStep > 0 ? handlePrevStep : undefined}
+          />
+          <ArrowRightCircle
+            className={getButtonClass(isValid && currStep < MAX_STEP)}
+            onClick={!isLastStep ? handleNextStep : undefined}
+          />
+        </div>
+        <div className="flex flex-1 justify-end">
+          {isLastStep && (
+            <Button
+              onClick={handleExternalSubmit}
+              className="rounded-[10px] bg-purple-500 px-[16px] py-[10px] text-white mobile1 hover:bg-purple-600">
+              등록하기
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
