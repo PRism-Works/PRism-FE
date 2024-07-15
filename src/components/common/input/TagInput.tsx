c977b8cd0245d25e137e88aee0425c704d2c0470
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-16 00:34:34 +0900

    refactor: TagInput thema 오타 수정, active->description으로 변경 (#37)

--- src/components/common/input/TagInput.tsx
+++ src/components/common/input/TagInput.tsx
@@ -9,23 +9,23 @@ type TagInputButton = 'delete' | 'add' | 'none';
 
 const colorConfig: Record<
   TagInputColor,
-  { base: string; active: string; hover: string; iconHover: string }
+  { base: string; description: string; hover: string; iconHover: string }
 > = {
   purple: {
     base: 'bg-purple-100 text-purple-500',
-    active: 'text-purple-300',
+    description: 'text-purple-300',
     hover: 'hover:bg-purple-200',
     iconHover: 'group-hover:text-purple-600',
   },
   indigo: {
-    base: 'bg-indigo-50 text-indigo-500',
-    active: 'text-indigo-300',
-    hover: 'hover:bg-indigo-100',
+    base: 'bg-indigo-100 text-indigo-500',
+    description: 'text-indigo-300',
+    hover: 'hover:bg-indigo-200',
     iconHover: 'group-hover:text-indigo-600',
   },
   gray: {
     base: 'bg-gray-100 text-gray-500',
-    active: 'text-gray-400',
+    description: 'text-gray-400',
     hover: 'hover:bg-gray-200',
     iconHover: 'group-hover:text-gray-600',
   },
@@ -34,7 +34,7 @@ const colorConfig: Record<
 interface TagInputProps {
   value: string;
   buttonType?: TagInputButton;
-  colorThema?: TagInputColor;
+  colorTheme?: TagInputColor;
   className?: string;
   onClick?: () => void;
 }
@@ -42,11 +42,11 @@ interface TagInputProps {
 export default function TagInput({
   value,
   buttonType = 'none',
-  colorThema = 'purple',
+  colorTheme = 'purple',
   className = '',
   onClick = () => {},
 }: TagInputProps) {
-  const { base, active, hover, iconHover } = colorConfig[colorThema];
+  const { base, description, hover, iconHover } = colorConfig[colorTheme];
 
   const handleTagClick = () => {
     if (onClick) onClick();
@@ -55,14 +55,14 @@ export default function TagInput({
   return (
     <div
       className={cn(
-        'group flex w-fit cursor-pointer items-center justify-between gap-1 rounded-[6px] px-1.5 py-1 transition-colors display5',
+        'group flex h-fit w-fit cursor-pointer items-center justify-between gap-1 rounded-[6px] px-1.5 py-1 transition-colors display5',
         base,
         hover,
         className,
       )}
       onClick={handleTagClick}>
       <span>#</span>
-      <span className={cn('whitespace-pre', buttonType === 'add' && active)}>{value}</span>
+      <span className={cn('whitespace-pre', buttonType === 'add' && description)}>{value}</span>
       <ButtonIcon buttonType={buttonType} iconHover={iconHover} />
     </div>
   );
