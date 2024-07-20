dfa08291c540ad64974c2968bf801e80f5b16bc3
yrk721 <yoorae0721@naver.com>
2024-07-20 21:34:12 +0900

    feat: GlobalFooter UI 수정 (#33)

--- src/app/layout/footer/GlobalFooter.tsx
+++ src/app/layout/footer/GlobalFooter.tsx
@@ -1,9 +1,18 @@
+import { Mail, Copyright } from 'lucide-react';
+
 export default function GlobalFooter() {
   return (
-    <footer className="bottom-0 h-[80px] w-full py-6 md:py-0">
-      <div className="gap-4 flex-col-center md:h-20 md:flex-row">
-        <p className="text-gray-600 body6">PRism</p>
-        <p className="text-gray-400 display5">©PRism. All rights reserved.</p>
+    <footer className="bottom-0 h-full w-full bg-gray-100 py-6">
+      <div className="flex flex-col items-center gap-2 md:h-20">
+        <p className="text-gray-600 display2">Team PRism.</p>
+        <div className="flex items-center gap-1 text-gray-500 mobile2">
+          <Mail className="h-4 w-4" />
+          Contact Us: swyp5thteam6@gmail.com
+        </div>
+        <div className="flex items-center text-gray-400 display5">
+          <Copyright className="h-3 w-3" />
+          2024. PRism. All rights reserved.
+        </div>
       </div>
     </footer>
   );
