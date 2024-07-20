8344a00788fc285627f2171489532a8dfeefcc6e
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-20 22:32:56 +0900

    feat: ShadowCard 클릭 이벤트 추가, TagInput disabled 옵션 추가 (#53)

--- src/components/common/card/ShadowCard.tsx
+++ src/components/common/card/ShadowCard.tsx
@@ -1,14 +1,16 @@
-import { ReactNode } from 'react';
 import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
 
 interface ShadowCardProps {
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-export default function ShadowCard({ children, className }: ShadowCardProps) {
+export default function ShadowCard({ children, className, onClick = () => {} }: ShadowCardProps) {
   return (
     <div
+      onClick={onClick}
       className={cn(
         'cursor-pointer rounded-[20px] bg-white p-4 shadow-custom-4px transition-shadow duration-300 hover:shadow-custom-16px active:bg-gray-50',
         className,
