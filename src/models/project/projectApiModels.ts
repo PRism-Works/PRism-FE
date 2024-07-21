d9d749b6390566182e0854adb26a7c3d902f56a2
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-21 15:52:26 +0900

    chore: 프로젝트 api models 분리, 나중에 api 확실해지면 공통 분리 예정 (#54)

--- src/models/project/projectApiModels.ts
+++ src/models/project/projectApiModels.ts
@@ -1,3 +1,4 @@
+// 서버에서 받아오는 카테고리 리스트 데이터 형식
 interface CategoryResponse {
   id: number;
   category: {
@@ -6,7 +7,8 @@ interface CategoryResponse {
   };
 }
 
-interface CommonProjectFields {
+// 프로젝트 등록
+export interface ProjectCreateRequest {
   projectName: string;
   organizationName: string;
   startDate: string;
@@ -15,46 +17,96 @@ interface CommonProjectFields {
   projectUrlLink: string;
   projectDescription: string;
   skills: string[];
-}
 
-export interface ProjectCreateRequest extends CommonProjectFields {
+  categories: string[];
   members: {
     name: string;
     email: string;
     roles: string[];
   }[];
-  categories: string[];
 }
 
 export interface ProjectCreateResponse {
   success: boolean;
   status: number;
-  data: CommonProjectFields & {
+  data: {
     projectId: number;
+
+    projectName: string;
+    organizationName: string;
+    startDate: string;
+    endDate: string;
+    memberCount: number;
+    projectUrlLink: string;
+    projectDescription: string;
+    skills: string[];
+
     categories: CategoryResponse[];
   };
 }
 
+// 프로젝트 수정
 // 프로젝트 산출물의 공개, 비공개 여부 추가 필요
-export interface ProjectUpdateRequest extends CommonProjectFields {
+export interface ProjectUpdateRequest {
+  projectName: string;
+  organizationName: string;
+  startDate: string;
+  endDate: string;
+  memberCount: number;
+  projectUrlLink: string;
+  projectDescription: string;
+  skills: string[];
+
+  categories: string[];
+
   members: {
     name: string;
     email: string;
     roles: string[];
   }[];
-  categories: string[]; // id 배열로 바뀔 예정
 }
 export interface ProjectUpdateResponse {
   success: boolean;
   status: number;
-  data: CommonProjectFields & {
+  data: {
     projectId: number;
+
+    projectName: string;
+    organizationName: string;
+    startDate: string;
+    endDate: string;
+    memberCount: number;
+    projectUrlLink: string;
+    projectDescription: string;
+    skills: string[];
+
     categories: CategoryResponse[];
   };
 }
 
+// 프로젝트 삭제
 export interface ProjectDeleteResponse {
   success: boolean;
   status: number;
   data: null;
+}
+
+// 등록한 프로젝트 리스트 가져오기
+export interface GetRegisteredProjectsResponse {
+  success: boolean;
+  status: number;
+  data: {
+    projectId: number;
+
+    projectName: string;
+    organizationName: string;
+    startDate: string;
+    endDate: string;
+
+    categories: string[];
+
+    visibility: boolean; // 필요 없는데..
+    userEvaluation: string; // 필요 없는데..
+    surveyParcitipants: number;
+  }[];
 }
