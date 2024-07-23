8f1fdc0bb34fbca4085e0d7328ca3a249415ed74
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-23 09:14:17 +0900

    chore: 연동 프로젝트 리스트 key 변경, projectList 값 할당부분 변경 (#79)

--- src/components/domain/project/projectList/LinkProjectList.tsx
+++ src/components/domain/project/projectList/LinkProjectList.tsx
@@ -13,22 +13,20 @@ interface LinkProjectListProps {
 export default function LinkProjectList({ searchProjectName }: LinkProjectListProps) {
   const { data, isLoading, isError } = useGetLinkProjectsByProjectName(searchProjectName);
 
-  const projectList = data?.data;
-  const projectDatas: ProjectSummaryData[] = !projectList
-    ? []
-    : projectList.map((project) => {
-        return {
-          projectId: project.projectId,
-          projectname: project.projectName,
-          organizationName: project.organizationName,
-          startDate: convertStringToDate(project.startDate),
-          endDate: convertStringToDate(project.endDate),
-          categories: project.categories,
-        };
-      });
+  const projectList: ProjectSummaryData[] =
+    data?.data?.map(
+      (project): ProjectSummaryData => ({
+        projectId: project.projectId,
+        projectname: project.projectName,
+        organizationName: project.organizationName,
+        startDate: convertStringToDate(project.startDate),
+        endDate: convertStringToDate(project.endDate),
+        categories: project.categories,
+      }),
+    ) || [];
 
   // 데이터가 정상적으로 가지고와진 상태인지 판단하여 다른 컴포넌트를 보여준다.
-  const isValidData = !(isLoading || projectDatas.length === 0 || isError);
+  const isValidData = !(isLoading || projectList.length === 0 || isError);
 
   return !isValidData ? (
     <BorderCard className="h-[165px] w-full flex-col-center">
@@ -38,14 +36,14 @@ export default function LinkProjectList({ searchProjectName }: LinkProjectListPr
         <span className="text-gray-600 display6">
           연동할 프로젝트를 로드하는 중 오류가 발생했습니다.
         </span>
-      ) : projectDatas.length === 0 ? (
+      ) : projectList.length === 0 ? (
         <span className="text-gray-600 display6">일치하는 검색 결과가 없습니다.</span>
       ) : null}
     </BorderCard>
   ) : (
     <ul className="flex flex-col gap-4">
-      {projectDatas.map((projectData, index) => (
-        <li key={index}>
+      {projectList.map((projectData) => (
+        <li key={projectData.projectId}>
           <ProjectSummaryCard
             variant={PROJECT_CARD_VARIANT.LINK_PREVIEW}
             projectData={projectData}
