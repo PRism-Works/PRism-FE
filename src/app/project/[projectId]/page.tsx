bbf2c97181369b4834e5968bd66b0dcd4269cbbf
jangseyeong <jang.se.yeong000@gmail.com>
2024-07-24 03:16:30 +0900

    feat: UserSummaryCard variant 추가, 검색 결과 프로젝트 상세조회 스크린 제작 (#56)

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -1,4 +1,13 @@
+'use client';
 // 검색 결과에서 넘어온 프로젝트 상세 조회 페이지
+// 이것도 페이지 전체가 서버에서 받아온 데이터가 필요해서.. nextjs의 fetch를 사용하는게 아니라면 .. ssr이 의미가 없을 것 같습니다..
+// use client 선언해서 사용하는게 좋을 것 같아요
+
+import BorderCard from '@/components/common/card/BorderCard';
+import TagInput from '@/components/common/input/TagInput';
+import UserSummaryCard from '@/components/domain/user/UserSummaryCard';
+import { USER_CARD_VARIANT } from '@/models/user/userModels';
+import { ArrowUpRight, CheckCircle2 } from 'lucide-react';
 
 interface SearchProjectDetailPageProps {
   params: { projectId: string };
@@ -6,11 +15,88 @@ interface SearchProjectDetailPageProps {
 
 export default function SearchProjectDetailPage({ params }: SearchProjectDetailPageProps) {
   const projectId = params.projectId;
-
+  console.log(projectId);
+  const testUserData = {
+    userId: '23fasdf',
+    name: '김프리즘',
+    email: 'rkfhadlwhgdk@naver.com',
+    roles: ['기획자', '디자이너'],
+  };
   return (
     <div className="container mx-auto flex min-h-screen flex-col items-center p-12">
-      <div className="w-full bg-gray-800">
-        프로젝트 검색 결과에서 넘어온 상세 조회, Project ID: {projectId}
+      <div className="flex w-full max-w-[1040px] flex-col gap-10">
+        <section className="flex flex-col gap-4">
+          <h2 className="text-gray-900 body6">프로젝트 정보</h2>
+          <BorderCard className="flex flex-col justify-center gap-7 p-7">
+            <h3 className="flex flex-col gap-1">
+              <div className="text-gray-500 caption">
+                <span className="text-purple-500 mobile2">적극성</span>이 높은 팀원이 많은 팀의
+                프로젝트에요!
+              </div>
+              <div className="flex items-center gap-3">
+                <span className="text-gray-700 body7">스위그 5기 6팀 팀프로젝트 PRism</span>
+                <span className="gap-1 text-gray-500 caption flex-center">
+                  <CheckCircle2 className="h-4 w-4 fill-success-50 stroke-success-500" />
+                  인증완료!
+                </span>
+              </div>
+            </h3>
+            <div className="grid grid-cols-[90px_1fr] gap-x-8 gap-y-7">
+              <div className="text-purple-800 display6">링크 바로가기</div>
+              <div className="flex items-center">
+                <a href="PRism.co.kr" className="text-gray-500 underline underline-offset-4">
+                  PRism.co.kr
+                </a>
+                <ArrowUpRight className="h-6 w-6 stroke-gray-500" />
+              </div>
+              <div className="text-gray-600 display6">기관명</div>
+              <div className="text-black display4">스위그</div>
+              <div className="text-gray-600 display6">기간</div>
+              <div className="text-black display4">2024.05.11 - 2024.07.22</div>
+            </div>
+          </BorderCard>
+        </section>
+        <section className="flex flex-col gap-4">
+          <h2 className="text-gray-900 body6">프로젝트 산출물 정보</h2>
+          <BorderCard className="p-7">
+            <div className="grid grid-cols-[90px_1fr] gap-x-8 gap-y-7">
+              <div className="text-gray-600 display6">상세설명</div>
+              <div className="text-black display4">
+                이 프로젝트는 웰훕스팅 커뮤니티 이 프로젝트는 웰훕스팅 커뮤니티 스위코에서 주최한
+                6주 단기 웹 서비스 출시를 목표로 진행된 프로젝트입니다. PRism은 it프로젝트를
+                마무리한 사람들에게 동료평가 서비스를 제공하는 웹사이트입니다. 주요 기능은
+                프로젝트를 등록하고 평가지를 보내 팀원들의 평가를 받아 5가지의 지표로 나타내는
+                것입니다.스위코에서 주최한 6주 단기 웹 서비스 출시를 목표로 진행된 프로젝트입니다.
+                PRism은 it프로젝트를 마무리한 사람들에게 동료평가 서비스를 제공하는 웹사이트입니다.
+                주요 기능은 프로젝트를 등록하고 평가지를 보내 팀원들의 평가를 받아 5가지의 지표로
+                나타내는 것입니다.
+              </div>
+
+              <div className="text-gray-600 display6">카테고리</div>
+              <div className="flex gap-2">
+                <TagInput value="금융" isDisabled />
+                <TagInput value="생산성" isDisabled />
+                <TagInput value="기타" isDisabled />
+              </div>
+
+              <div className="text-gray-600 display6">기술스택</div>
+              <div className="flex gap-2">
+                <TagInput value="Spring Framework" isDisabled colorTheme="gray" />
+                <TagInput value="Python" isDisabled colorTheme="gray" />
+                <TagInput value="HTML/CSS" isDisabled colorTheme="gray" />
+              </div>
+            </div>
+          </BorderCard>
+        </section>
+        <section className="flex flex-col gap-4">
+          <h2 className="text-gray-900 body6">팀원 정보</h2>
+          <div className="flex flex-wrap gap-4">
+            <UserSummaryCard userData={testUserData} />
+            <UserSummaryCard userData={testUserData} variant={USER_CARD_VARIANT.NON_MEMBER} />
+            <UserSummaryCard userData={testUserData} variant={USER_CARD_VARIANT.MEMBER_PRIVATE} />
+            <UserSummaryCard userData={testUserData} variant={USER_CARD_VARIANT.MEMBER_PUBLIC} />
+          </div>
+        </section>
       </div>
     </div>
   );
--- src/components/domain/user/UserSummaryCard.tsx
+++ src/components/domain/user/UserSummaryCard.tsx
@@ -1,37 +1,64 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import ShadowCard from '@/components/common/card/ShadowCard';
 import TagInput from '@/components/common/input/TagInput';
 import CirclePlanetIcon from './CirclePlanetIcon';
 import { ChevronRight } from 'lucide-react';
+import {
+  USER_CARD_VARIANT,
+  type UserSummaryCardVariant,
+  type UserSummaryData,
+} from '@/models/user/userModels';
+import { maskEmail, maskName } from '@/lib/masking';
 
-export default function UserSummaryCard() {
+interface UserSummaryCard {
+  userData: UserSummaryData;
+  variant?: UserSummaryCardVariant;
+}
+
+export default function UserSummaryCard({
+  userData,
+  variant = USER_CARD_VARIANT.MEMBER_PUBLIC,
+}: UserSummaryCard) {
+  const isPublicUser = variant === USER_CARD_VARIANT.MEMBER_PUBLIC;
   const handleOpenUserProfile = () => {
-    console.log('프로필 보러가기 연결 예정');
+    if (!isPublicUser) return;
+    alert(`${userData.userId}번 유저의 프로필 보러가기`);
   };
 
-  // NOTE: project API 연동 이후 하드코딩된 값 변경 예정
-
   return (
-    <ShadowCard className="flex h-[157px] w-[512px] flex-col justify-between p-6">
+    <ShadowCard
+      className={cn(
+        'flex h-[157px] w-[512px] flex-col justify-between p-6',
+        !isPublicUser && 'cursor-default',
+      )}
+      onClick={handleOpenUserProfile}>
       <div className="flex items-start space-x-4">
         <CirclePlanetIcon className="bg-gray-100" />
         <div className="flex flex-col justify-center">
-          <p className="body8">이지영</p>
-          <p className="text-gray-500 display5">fj298@gmail.com</p>
+          <p className="body8">
+            {variant === USER_CARD_VARIANT.MEMBER_PRIVATE ? maskName(userData.name) : userData.name}
+          </p>
+          <p className="text-gray-500 display5">
+            {isPublicUser ? userData.email : maskEmail(userData.email)}
+          </p>
         </div>
       </div>
       <div className="mt-auto flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <TagInput value="기획자" buttonType="none" colorTheme="indigo" />
-          <TagInput value="디자이너" buttonType="none" colorTheme="indigo" />
-        </div>
-        <div
-          className="cursor-pointer text-gray-600 underline decoration-current underline-offset-4 display5 flex-center"
-          onClick={handleOpenUserProfile}>
-          프로필 보러가기
-          <ChevronRight className="h-4 w-4" />
-        </div>
+        <ul className="flex items-center gap-2">
+          {userData.roles.map((role, index) => (
+            <li key={index}>
+              <TagInput value={role} isDisabled colorTheme="indigo" />
+            </li>
+          ))}
+        </ul>
+        {isPublicUser && (
+          <div className="cursor-pointer text-gray-600 underline decoration-current underline-offset-4 display5 flex-center">
+            프로필 보러가기
+            <ChevronRight className="h-4 w-4" />
+          </div>
+        )}
       </div>
     </ShadowCard>
   );
