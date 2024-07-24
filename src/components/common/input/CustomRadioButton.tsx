04c683b71ba293738986da39d6aea9df43f81bfb
yrk721 <yoorae0721@naver.com>
2024-07-24 22:43:08 +0900

    feat: 평가지에서 쓰이는 커스텀 라디오버튼 컴포넌트 생성 (#68)

---
+++ src/components/common/input/CustomRadioButton.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { UseFormRegister } from 'react-hook-form';
+
+interface CustomRadioButtonProps {
+  name: string;
+  value: string;
+  register: UseFormRegister<Record<string, unknown>>;
+}
+
+export default function CustomRadioButton({ name, value, register }: CustomRadioButtonProps) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsSelected(event.target.checked);
+    console.log(`Radio button ${name} with value ${value} selected: ${event.target.checked}`);
+  };
+
+  return (
+    <label className="relative flex cursor-pointer items-center space-x-4">
+      <input
+        type="radio"
+        {...register(name)}
+        value={value}
+        className="hidden"
+        onChange={handleChange}
+      />
+      <div className="relative flex h-6 w-6 items-center justify-center">
+        <div
+          className={`absolute h-full w-full rounded-full border-2 ${
+            isSelected ? 'border-purple-500' : 'border-gray-300'
+          }`}></div>
+        <div
+          className={`h-4 w-4 rounded-full ${isSelected ? 'bg-purple-500' : 'bg-gray-300'}`}></div>
+      </div>
+    </label>
+  );
+}
