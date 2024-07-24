901cb61d3a0f39406375ea5d5804b8947c022fd7
yrk721 <yoorae0721@naver.com>
2024-07-25 04:39:38 +0900

    feat: CarouselPrevious/CarouselNext form 밖으로 이동 #68()

--- src/app/survey/[projectId]/[emailId]/page.tsx
+++ src/app/survey/[projectId]/[emailId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { SurveyStep } from '@/models/survey/surveyModels';
 import { questions } from '@/lib/surveyQuestion';
@@ -19,7 +19,7 @@ import { Button } from '@/components/ui/button';
 
 const teamMembers = ['김이브', '나동현', '유혜성', '이나윤', '우지희', '박민수', '안유경'];
 
-export default function SurveyPage(): JSX.Element {
+export default function SurveyPage() {
   const [current, setCurrent] = useState<number>(1);
   const [api, setApi] = useState<CarouselApi | null>(null);
   const methods = useForm<Record<string, unknown>>();
@@ -52,14 +52,14 @@ export default function SurveyPage(): JSX.Element {
   // 평가지 제출 로직 추가 예정
   const onSubmit = (data: Record<string, unknown>) => {
     console.log(data);
-    // alert('평가를 제출할까요?');
+    alert('평가를 제출할까요?');
   };
 
   return (
     <div className="container min-h-screen w-full max-w-[1040px] p-4 flex-col-center">
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full max-w-[1040px]">
-          <Carousel setApi={setApi} className="relative mx-auto w-full max-w-[1040px]">
+      <Carousel setApi={setApi} className="relative mx-auto w-full max-w-[1040px]">
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full max-w-[1040px]">
             <CarouselContent>
               {steps.map((step, index) => {
                 const StepComponent = step.component;
@@ -76,19 +76,19 @@ export default function SurveyPage(): JSX.Element {
                 );
               })}
             </CarouselContent>
-            <div className="flex-center">
-              <CarouselPrevious className="h-10 w-10" />
-              {current === steps.length ? (
-                <Button type="submit" className="-mt-9 h-[50px] w-[100px] cursor-pointer body8">
-                  등록하기
-                </Button>
-              ) : (
-                <CarouselNext className="h-10 w-10" />
-              )}
-            </div>
-          </Carousel>
-        </form>
-      </FormProvider>
+          </form>
+        </FormProvider>
+        <div className="flex-center">
+          <CarouselPrevious className="h-10 w-10" />
+          {current === steps.length ? (
+            <Button type="submit" className="-mt-9 h-[50px] w-[100px] cursor-pointer body8">
+              등록하기
+            </Button>
+          ) : (
+            <CarouselNext className="h-10 w-10" />
+          )}
+        </div>
+      </Carousel>
     </div>
   );
 }
