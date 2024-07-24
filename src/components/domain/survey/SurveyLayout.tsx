44acb1342ab7b7cb7bd475b7222b706f3f31748f
yrk721 <yoorae0721@naver.com>
2024-07-24 22:44:29 +0900

    feat: 평가지 레이아웃 생성 (#68)

---
+++ src/components/domain/survey/SurveyLayout.tsx
@@ -0,0 +1,35 @@
+import ProgressBar from '@/components/common/progressBar/ProgressBar';
+
+interface SurveyLayoutProps {
+  children: React.ReactNode;
+  currentStep: number;
+  totalSteps: number;
+  question: string;
+  stepNumber: number;
+}
+
+export default function SurveyLayout({
+  children,
+  currentStep,
+  totalSteps,
+  question,
+  stepNumber,
+}: SurveyLayoutProps) {
+  const progressPercent = (currentStep / totalSteps) * 100;
+
+  return (
+    <div className="mx-auto max-h-[900px] w-full max-w-[1040px] overflow-y-auto p-4 md:p-8">
+      <div className="mb-2 flex-center">
+        <div className="mr-3 h-8 w-8 rounded-full bg-black text-white display6 flex-center">
+          {stepNumber}
+        </div>
+        <p className="text-purple-800 body2">{question}</p>
+      </div>
+      <div className="mb-4">
+        <div className="text-right text-purple-700 body8">{`${currentStep}/${totalSteps}`}</div>
+        <ProgressBar percent={progressPercent} />
+      </div>
+      <div className="flex-grow">{children}</div>
+    </div>
+  );
+}
