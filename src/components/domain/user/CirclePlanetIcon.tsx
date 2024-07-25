627d4e967ea797eed204da08ff078a9770bd4570
yrk721 <yoorae0721@naver.com>
2024-07-25 23:06:37 +0900

    feat: ComponentSpinner 예외처리 제거 (#93)

--- src/components/domain/user/CirclePlanetIcon.tsx
+++ src/components/domain/user/CirclePlanetIcon.tsx
@@ -2,30 +2,20 @@
 
 import { cn } from '@/lib/utils';
 import { PlanetIcons } from '@/assets/icon/planet';
-import { ComponentSpinner } from '@/components/common/spinner';
 
 interface CirclePlanetIconProps {
   className?: string;
   iconIndex?: number;
 }
 
-export default function CirclePlanetIcon({ className, iconIndex }: CirclePlanetIconProps) {
+export default function CirclePlanetIcon({ className, iconIndex = 0 }: CirclePlanetIconProps) {
   const planetKeys = Object.keys(PlanetIcons) as Array<keyof typeof PlanetIcons>;
-  const PlanetIcon =
-    iconIndex !== undefined
-      ? PlanetIcons[planetKeys[iconIndex % planetKeys.length]]
-      : PlanetIcons[planetKeys[0]];
+  const PlanetIcon = PlanetIcons[planetKeys[iconIndex % planetKeys.length]];
 
   return (
     <div
       className={cn('flex h-[56px] w-[56px] items-center justify-center rounded-full', className)}>
-      {PlanetIcon ? (
-        <PlanetIcon />
-      ) : (
-        <div className="h-[56px] w-[56px] flex-center">
-          <ComponentSpinner />
-        </div>
-      )}
+      {PlanetIcon && <PlanetIcon />}
     </div>
   );
 }
