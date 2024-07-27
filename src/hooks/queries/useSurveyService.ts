4462fcdcafc5d7b8dcb07a9a1c672f0afa8c866a
yrk721 <yoorae0721@naver.com>
2024-07-28 02:36:49 +0900

    feat: useSurveyService 생성 (#95)

---
+++ src/hooks/queries/useSurveyService.ts
@@ -0,0 +1,54 @@
+import { AxiosError } from 'axios';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchSurveyLink, sendSurveyLink, submitSurvey } from '@/services/api/surveyApi';
+import type {
+  SurveyLinkRequest,
+  SurveyLinkResponse,
+  SendSurveyLinkRequest,
+  SendSurveyLinkResponse,
+  SubmitSurveyRequest,
+  SubmitSurveyResponse,
+} from '@/models/survey/surveyApiModels';
+
+// 설문 링크 가져오기
+export const useFetchSurveyLink = (params: SurveyLinkRequest) => {
+  return useQuery<SurveyLinkResponse, AxiosError>({
+    queryKey: ['fetchSurveyLink', params],
+    queryFn: () => fetchSurveyLink(params),
+    enabled: !!params.code, // code가 존재할 때만 쿼리 실행
+  });
+};
+
+// 설문 링크 보내기
+export const useSendSurveyLink = (successCallback: () => void) => {
+  return useMutation<SendSurveyLinkResponse, AxiosError, SendSurveyLinkRequest>({
+    mutationFn: sendSurveyLink,
+    onSuccess: (response) => {
+      console.log('Send Survey Link Response:', response);
+      if (successCallback) successCallback();
+    },
+    onError: (error) => {
+      console.error('Send Survey Link Error:', error);
+      alert('평가 링크 발송에 실패했습니다.');
+    },
+  });
+};
+
+// 설문 응답 제출하기
+export const useSubmitSurvey = (successCallback: () => void) => {
+  return useMutation<
+    SubmitSurveyResponse,
+    AxiosError,
+    { projectId: number; data: SubmitSurveyRequest }
+  >({
+    mutationFn: ({ projectId, data }) => submitSurvey(projectId, data),
+    onSuccess: (response) => {
+      console.log('Submit Survey Response:', response);
+      if (successCallback) successCallback();
+    },
+    onError: (error) => {
+      console.error('Submit Survey Error:', error);
+      alert('평가 제출에 실패했습니다.');
+    },
+  });
+};
