b428235fee24cd32fcf1b2c69854a73438e38f93
yrk721 <yoorae0721@naver.com>
2024-08-08 00:59:18 +0900

    feat: CirclePlanetIcon 다크모드 적용 (#140)

--- src/components/domain/user/CirclePlanetIcon.tsx
+++ src/components/domain/user/CirclePlanetIcon.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { useTheme } from 'next-themes';
 import { PlanetIcons } from '@/assets/icon/planet';
 
 interface CirclePlanetIconProps {
@@ -9,13 +10,20 @@ interface CirclePlanetIconProps {
 }
 
 export default function CirclePlanetIcon({ className, iconIndex = 0 }: CirclePlanetIconProps) {
+  const { theme } = useTheme();
+  const isDarkMode = theme === 'dark';
+
   const planetKeys = Object.keys(PlanetIcons) as Array<keyof typeof PlanetIcons>;
   const PlanetIcon = PlanetIcons[planetKeys[iconIndex % planetKeys.length]];
+
+  const iconStyle = {
+    filter: isDarkMode ? 'invert(1) hue-rotate(180deg) brightness(0.9)' : 'none',
+  };
 
   return (
     <div
       className={cn('flex h-[56px] w-[56px] items-center justify-center rounded-full', className)}>
-      {PlanetIcon && <PlanetIcon />}
+      {PlanetIcon && <PlanetIcon style={iconStyle} />}
     </div>
   );
 }
