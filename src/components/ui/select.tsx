112fd211ced5455e26c2b429e2b8f3b680050805
yoorae <yoorae0721@naver.com>
2024-12-03 18:01:59 +0900

    style: ui/select 컴포넌트 디자인 시스템 적용 (#191)

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,15 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex h-[45px] w-full items-center justify-between',
+      'rounded-[10px] border border-gray-400',
+      'bg-background px-3 py-2 text-sm',
+      'focus:border-purple-500 focus:outline-none',
+      'disabled:cursor-not-allowed disabled:opacity-50',
+      '[&>span]:line-clamp-1',
+      'placeholder:text-gray-400',
+      '[&>span[data-placeholder]]:text-gray-400',
+      'dark:text-gray-500 dark:border-gray-500 dark:bg-black/20 dark:focus:border-purple-500',
       className,
     )}
     {...props}>
