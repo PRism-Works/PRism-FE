23be3052b459112409fbeee376dfd868885b9cf7
yoorae <yoorae0721@naver.com>
2024-12-10 00:44:33 +0900

    feat: RichTextEditor 컴포넌트 생성 (#191)

--- src/components/domain/team/recruit/section/DetailsSection.tsx
+++ src/components/domain/team/recruit/section/DetailsSection.tsx
@@ -1,8 +1,7 @@
 import { useFormContext } from 'react-hook-form';
 import { TeamRecruitFormValues } from '@/models/team/teamModels';
 import { FormField, FormItem, FormControl, FormMessage } from '@/components/ui/form';
-import { Textarea } from '@/components/ui/textarea';
-import { cn } from '@/lib/utils';
+import RichTextEditor from '@/components/common/textEditor/RichTextEditor';
 
 export default function DetailsSection() {
   const form = useFormContext<TeamRecruitFormValues>();
@@ -14,10 +13,14 @@ export default function DetailsSection() {
       render={({ field, fieldState: { error } }) => (
         <FormItem className="relative space-y-3">
           <FormControl>
-            <Textarea
+            <RichTextEditor
+              value={field.value || ''}
+              name={field.name}
+              ref={field.ref}
+              onBlur={field.onBlur}
+              onChange={field.onChange}
+              error={!!error}
               placeholder="팀빌딩을 위한 상세 정보를 입력해 주세요."
-              className={cn('min-h-[200px] w-full', error && 'border-red-500')}
-              {...field}
             />
           </FormControl>
           <FormMessage className="absolute -bottom-6 ml-1" />
